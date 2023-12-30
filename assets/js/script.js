e24ca22f29fd25d3f07354665edf3ec24e5525d9
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2023-12-30 00:11:27 -0500

    pulled lat/lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
 const API_KEY = '8823600ae11757d74ec67f06b60ca5ef';
-const BASE_PATH ='http://api.openweathermap.org/';
-const GEO_PATH = '/geo/1.0/direct'
+const BASE_PATH = 'http://api.openweathermap.org';
+const GEO_PATH = '/geo/1.0/direct';
 
+const testLat = 35.2272086;
+const testLon = -80.8430827;
 
-function getGeocoding () {
-    
+function getGeocoding() {
+  fetch(`${BASE_PATH}${GEO_PATH}?appid=${API_KEY}&limit=1&q=Charlotte`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+    });
 }
+
+getGeocoding();
+
+
