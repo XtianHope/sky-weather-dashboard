fafcdc0cf1a2ce929c10ed3fc168ec02a650944b
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2023-12-30 00:22:03 -0500

    built out functions for getGeocoding, getCurrentWeather, & getFiveDayForecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 const API_KEY = '8823600ae11757d74ec67f06b60ca5ef';
 const BASE_PATH = 'http://api.openweathermap.org';
 const GEO_PATH = '/geo/1.0/direct';
+const WEATHER_PATH = '/data/2.5/weather';
+const FIVE_DAY_PATH= '/data/2.5/forecast';
 
 const testLat = 35.2272086;
 const testLon = -80.8430827;
+const testCity = 'Charlotte';
 
 function getGeocoding() {
-  fetch(`${BASE_PATH}${GEO_PATH}?appid=${API_KEY}&limit=1&q=Charlotte`)
+  fetch(`${BASE_PATH}${GEO_PATH}?appid=${API_KEY}&limit=1&q=${testCity}`)
     .then(function(response) {
       return response.json();
     })
@@ -15,6 +18,23 @@ function getGeocoding() {
     });
 }
 
-getGeocoding();
+function getCurrentWeather() {
+    fetch(`${BASE_PATH}${WEATHER_PATH}?appid=${API_KEY}&lat=${testLat}&lon=${testLon}&units=imperial`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+      });
+  }
 
+  function getFiveDayForecast() {
+    fetch(`${BASE_PATH}${FIVE_DAY_PATH}?appid=${API_KEY}&lat=${testLat}&lon=${testLon}&units=imperial`)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+      });
+  }
 
