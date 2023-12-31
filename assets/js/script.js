80ee6dc7bab84214c11e248df01d0e70e931e32a
Christian Tino <146997867+XtianHope@users.noreply.github.com>
2023-12-30 21:34:34 -0500

    updated functions and included event listener for submit button and added weather icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,22 +12,26 @@ const fiveDayContainer = document.querySelector('#five-day');
 
 
 
-const testLat = 35.2272086;
-const testLon = -80.8430827;
-const testCity = 'Charlotte';
-
-function getGeocoding() {
-  fetch(`${BASE_PATH}${GEO_PATH}?appid=${API_KEY}&limit=1&q=${testCity}`)
+function getGeocoding(city) {
+  fetch(`${BASE_PATH}${GEO_PATH}?appid=${API_KEY}&limit=1&q=${city}`)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      console.log(data);
+        console.log(data);
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        getCurrentWeather(lat, lon);
+        getFiveDayForecast(lat, lon);
+        const cityButton = document.createElement('button');
+        cityButton.textContent = city;
+        cityButton.className = 'btn btn-lg btn-secondary mb-3 w-100'
+        cityContainer.appendChild(cityButton);
     });
 }
 
-function getCurrentWeather() {
-    fetch(`${BASE_PATH}${WEATHER_PATH}?appid=${API_KEY}&lat=${testLat}&lon=${testLon}&units=imperial`)
+function getCurrentWeather(lat, lon) {
+    fetch(`${BASE_PATH}${WEATHER_PATH}?appid=${API_KEY}&lat=${lat}&lon=${lon}&units=imperial`)
       .then(function(response) {
         return response.json();
       })
@@ -47,15 +51,15 @@ function getCurrentWeather() {
 
 
 
-  function getFiveDayForecast() {
-    fetch(`${BASE_PATH}${FIVE_DAY_PATH}?appid=${API_KEY}&lat=${testLat}&lon=${testLon}&units=imperial`)
+  function getFiveDayForecast(lat, lon) {
+    fetch(`${BASE_PATH}${FIVE_DAY_PATH}?appid=${API_KEY}&lat=${lat}&lon=${lon}&units=imperial`)
       .then(function(response) {
         return response.json();
       })
       .then(function(data) {
-        fiveDayContainer.innerHTML = ''; // Clear the container before adding new content
+        fiveDayContainer.innerHTML = null;
   
-        for (let i = 0; i < data.list.length; i += 8) {
+        for (let i = 3; i < data.list.length; i += 8) {
           const weather = data.list[i];
           const colEl = document.createElement('div');
           colEl.className = 'col-12 col-xl';
@@ -73,6 +77,12 @@ function getCurrentWeather() {
           h4El.textContent = weather.dt_txt;
           h4El.className = 'card-title';
           cardBody.appendChild(h4El);
+
+          const icon = weather.weather[0].icon;
+          const iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
+          const iconImg = document.createElement('img');
+          iconImg.src = iconUrl;
+          cardBody.appendChild(iconImg);
   
           const dataLines = [weather.main.temp, weather.main.humidity, weather.wind.speed];
           for (const line of dataLines) {
@@ -84,4 +94,11 @@ function getCurrentWeather() {
       });
   }
 
-  getFiveDayForecast();
+
+
+
+
+  searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    getGeocoding(inputEl.value.trim());
+  });
